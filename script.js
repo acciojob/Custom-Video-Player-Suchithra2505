c5dc04c9d7dba7546c4143bd5d8f45a02b61034e
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-01-02 19:46:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,57 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const playbackSpeed = player.querySelector('[name="playbackRate"]');
+const volumeRange = player.querySelector('[name="volume"]');
+const backwardButton = player.querySelector('.backward');
+const forwardButton = player.querySelector('.forward');
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function skipTime(seconds) {
+  video.currentTime += seconds;
+}
+
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+volumeRange.addEventListener('input', handleRangeUpdate);
+playbackSpeed.addEventListener('input', handleRangeUpdate);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+backwardButton.addEventListener('click', () => skipTime(-10));
+forwardButton.addEventListener('click', () => skipTime(25));
